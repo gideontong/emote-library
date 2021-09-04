f8142d41c9c3765937e03c3f30f43da89b2ebacf
Gideon Tong <gideon@gideontong.com>
2021-09-04 00:12:22 -0700

    Prepare directories

--- emotify/__init__.py
+++ emotify/__init__.py
@@ -1,5 +1,11 @@
 from emotify.convert import convert_to_all_formats
+from os import listdir
+from os.path import isdir
 
 
-def auto_convert_everything(path: str):
-    convert_to_all_formats()
+def auto_convert_everything(import_folder: str):
+    if isdir(import_folder):
+        pass
+    else:
+        # TODO: Do something if the parameter is bad
+        pass
