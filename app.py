6c91d45ffc6332c919bc9454404d70faaba23360
Gideon Tong <gideon@gideontong.com>
2021-09-03 22:58:26 -0700

    Move emotes out

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+from argparse import ArgumentParser
+
+
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser(description='Make emotes fast again!')
+    parser.add_argument('--disableauto', action='store_true', help='Disable automatic parsing')
+    args = parser.parse_args()
