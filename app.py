793ea7a6356fd0e1622d4c00f1289f2fa0117c94
Gideon Tong <gideon@gideontong.com>
2021-09-03 23:03:50 -0700

    Prepare format

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Make emotes fast again!')
-    parser.add_argument('--disableauto', action='store_true', help='Disable automatic parsing')
+    parser.add_argument('--disableauto', action='store_true',
+                        help='Disable automatic parsing')
     args = parser.parse_args()
