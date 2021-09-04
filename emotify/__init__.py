9d3049c35e455b6edcdb3e61ee22451f8ac9ebd9
Gideon Tong <gideon@gideontong.com>
2021-09-03 23:07:40 -0700

    Prepare conversions

---
+++ emotify/__init__.py
@@ -0,0 +1,5 @@
+from emotify.convert import convert_to_all_formats
+
+
+def auto_convert_everything(path: str):
+    convert_to_all_formats()
---
+++ emotify/convert.py
@@ -0,0 +1,10 @@
+FORMATS = [32, 64, 128]
+
+
+def convert_to_size(width: int):
+    pass
+
+
+def convert_to_all_formats():
+    for width in FORMATS:
+        convert_to_size(width)
